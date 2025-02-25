b3e4416e448654faedb1dc21343bfabfca11291f
Aniket Tiwari <pt5602238@gmail.com>
2025-02-25 11:03:06 +0530

    button added

--- 05github/first.js
+++ 05github/first.js
@@ -1,4 +1,15 @@
 const button = "Added a buttton ";
 console.log(button);
 
+const  login = "login page added ";
+console.log(login);
+
+const payment  ="integrated the payement";
+console.log(payment);
+
+const upi ="upi system added";
+console.log(upi);
+
+
+
 
